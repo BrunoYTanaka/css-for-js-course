a716ab407aeaf610ed6ecc53b53d5c625d696e6c
Bruno Y. Tanaka <brunoyoichi@hotmail.com>
2023-05-31 07:53:26 -0400

    Workshop: Mini Components Library

--- mini-component-library/src/components/IconInput/IconInput.js
+++ mini-component-library/src/components/IconInput/IconInput.js
@@ -6,40 +6,77 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
-const SIZES = {
+const STYLES = {
   small: {
-    "--border-bottom": `1px solid ${COLORS.black}`,
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
   },
   large: {
-    "--border-bottom": `2px solid ${COLORS.black}`,
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
   },
 };
 
-const ICON_SIZES = {
-  small: 10,
-  large: 16,
-}
-
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
-  const styles = SIZES[size];
+  const styles = STYLES[size];
 
   return (
-    <IconInputWrapper style={styles} width={width}>
-      <Icon id={icon} size={ICON_SIZES[size]}/>
-      <Input type="text" placeholder={placeholder} />
+    <IconInputWrapper style={styles}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        type="text"
+        placeholder={placeholder}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height / 16 + "rem",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize / 16 + "rem",
+        }}
+      />
     </IconInputWrapper>
   );
 };
 
-const IconInputWrapper = styled.div`
+const IconInputWrapper = styled.label`
   position: relative;
   display: inline;
-  width: ${({ width }) => `${width}px`};
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
 `;
 
-const Input = styled.input`
+const TextInput = styled.input`
+  height: var(--height);
+  width: var(--width);
+  font-size: var(--font-size);
   border: none;
-  border-bottom: var(--border-bottom);
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color : inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color : ${COLORS.gray500}
+    font-weight: 400;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
 `;
 
 export default IconInput;
